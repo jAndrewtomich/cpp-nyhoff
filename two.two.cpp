3277d3f7262aa06f7cb3e9150012f9c24cefc41d
jatomic <johnatomich@gmail.com>
2022-02-21 15:35:46 -0600

    Solution

--- two.two.cpp
+++ two.two.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <math.h>
 
-void printBinary(int &n){
-    std::cout << sizeof(n) << '\n';
+void printBinary(int n){
+
+    int num_of_bits = sizeof(n) * 8;
+    int binary_representation[num_of_bits];
+
+    uint mask = pow(2, num_of_bits - 1);
+
+    for (int i = 0; i < num_of_bits; ++i){
+        if (n & mask)
+            binary_representation[i] = 1;
+        else
+            binary_representation[i] = 0;
+        mask = mask>>1;
+    }
+
+    for (int j = 0; j < num_of_bits; ++j)
+        std::cout << binary_representation[j];
+
+    std::cout << std::endl;    
 }
 
 
